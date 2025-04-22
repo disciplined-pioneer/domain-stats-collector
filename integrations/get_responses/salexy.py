46d862651b525c3b8d65024abd20f73392268a18
Virtu <maksimsarsov777@gmail.com>
2025-04-22 21:05:59 +0300

    Настроены запросы к salexy

---
+++ integrations/get_responses/salexy.py
@@ -0,0 +1,82 @@
+import time
+import asyncio
+import aiohttp
+import pandas as pd
+
+class SalexyFetcher:
+
+    """Класс для получения данных с сайтов по их доменам - SALEXY"""
+
+    def __init__(self, domains):
+        self.domains = domains
+        self.results = []
+
+    # Асинхронная функция для выполнения запросов и сбора данных в DataFrame
+    async def fetch_pwa_stats(self):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            
+            for domain in self.domains:
+                api_url = f"https://{domain}/custom-api/v1/pwa-install-stats"
+                tasks.append(self.fetch_for_domain(session, domain, api_url))
+            await asyncio.gather(*tasks)
+
+        # Создание DataFrame из собранных результатов
+        df = pd.DataFrame(self.results)
+        return df
+
+    # Функция для получения данных с каждого домена
+    async def fetch_for_domain(self, session, domain, url):
+        try:
+            # Отправка GET-запроса
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    result = {
+                        'Timestamp': time.time(),
+                        'Website': domain,
+                        'daily': data.get('daily', 'error'),
+                        'weekly': data.get('weekly', 'error'),
+                        'monthly': data.get('monthly', 'error'),
+                        'Status': 'ОК!'
+                    }
+                else:
+                    result = {
+                        'Timestamp': time.time(),
+                        'Website': domain,
+                        'daily': '-',
+                        'weekly': '-',
+                        'monthly': '-',
+                        'Status': '❌ ERROR'
+                    }
+                    print(f"❌ Ошибка для {domain}: {response.status}")
+                self.results.append(result)
+
+        except Exception as e:
+            result = {
+                'Timestamp': time.time(),
+                'Website': domain,
+                'daily': '-',
+                'weekly': '-',
+                'monthly': '-',
+                'Status': '❌ ERROR'
+            }
+            self.results.append(result)
+            print(f"❌ Ошибка для {domain}: {e}")
+
+
+# Пример использования
+if __name__ == "__main__":
+
+    domains = [
+        "salexy.kz"
+    ]
+
+    # Инициализация класса с доменами
+    fetcher = SalexyFetcher(domains)
+    
+    # Получение DataFrame с результатами
+    df = asyncio.run(fetcher.fetch_pwa_stats())
+    
+    # Вывод результата
+    print(df)
