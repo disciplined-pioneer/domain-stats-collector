1c511bdcb494c09e6ee04f9ca28e7517a9fe4347
Virtu <maksimsarsov777@gmail.com>
2025-04-23 14:50:42 +0300

    Настроены стили при создании таблиицы

--- integrations/google_sheets/google_sheets.py
+++ integrations/google_sheets/google_sheets.py
@@ -2,6 +2,7 @@ import gspread
 from typing import List
 from settings import settings
 from google.oauth2.service_account import Credentials
+from gspread_formatting import format_cell_range, CellFormat, Color, TextFormat
 
 
 # Настраиваем доступ к Google Sheets через google-auth
@@ -20,20 +21,34 @@ def authorize_spreadsheet():
     return client.open(settings.bot.SHEETS_NAME)
 
 
-# Создание  нужных листов, согласно списку
+# Создание  нужных листов, согласно списку + настройка стилей
 def create_sheets(list_sheets: List[str] = ["Daily_Stats", "Weekly_Stats", "Monthly_Stats"]) -> None:
-
     spreadsheet = authorize_spreadsheet()
 
-    # Проходим по списку листов и создаем каждый
+    # Настройка стилей
+    header_format = CellFormat(
+        backgroundColor=Color(209 / 255, 109 / 255, 107 / 255),
+        textFormat=TextFormat(
+            foregroundColor=Color(1, 1, 1),  # Белый текст
+            bold=True
+        ),
+        horizontalAlignment="CENTER",
+        verticalAlignment="MIDDLE"
+    )
+
+    # Проходимся по всем листам
     for sheet in list_sheets:
         try:
-            # Проверяем, существует ли уже лист с таким названием
             spreadsheet.add_worksheet(title=sheet, rows="100", cols="20")
-            print(f"Лист '{sheet}' успешно создан.")
+            worksheet = spreadsheet.worksheet(sheet)
+            worksheet.update("A1", [["Timestamp", "Website", "Count", "Delta", "Status"]])
+            format_cell_range(worksheet, 'A1:E1', header_format)
+            print(f"Лист '{sheet}' успешно создан и отформатирован.")
 
         except gspread.exceptions.APIError as e:
-            # Если лист с таким названием уже существует
             print(f"Лист '{sheet}' уже существует или произошла ошибка: {e}")
+
+        except Exception as e:
+            print(f"Произолша ошибка: {e}")
 
     print("Листы успешно созданы.")
